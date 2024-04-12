3454612c95ca672dce78a10bcd7bbcedb2a74712
Pradip <dellpidarp57@gmail.com>
2024-04-12 12:41:51 +0545

    validation of register controller using joi

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,9 +1,27 @@
+const Joi = require("joi");
+
 class AuthController {
   register = (req, res, next) => {
     const payload = req.body;
 
+    const rule = Joi.object({
+      name: Joi.string().min(3).max(20).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string()
+        .pattern(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,15}$/
+        )
+        .required(),
+      confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+      role: Joi.string()
+        .pattern(/^(staff|customer)$/)
+        .required(),
+    });
+
+    const response = rule.validate(payload, { abortEarly: false });
+
     res.json({
-      result: payload,
+      result: response,
       message: "Successful Register",
       meta: null,
     });

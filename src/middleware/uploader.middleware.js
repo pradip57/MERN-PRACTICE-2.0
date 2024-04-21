585e208626b330efc71e79471e32e334e88f8be5
Pradip <dellpidarp57@gmail.com>
2024-04-21 20:26:51 +0545

    fileFilter for imageFilter

--- src/middleware/uploader.middleware.js
+++ src/middleware/uploader.middleware.js
@@ -1,38 +1,42 @@
-const multer = require('multer')
-const fs = require('fs')
-const { generateRandomString } = require('../utilities/helpers')
+const multer = require("multer");
+const fs = require("fs");
+const { generateRandomString } = require("../utilities/helpers");
 
 const setPath = (path) => {
-    return (req,res,next) => {
-        req.uploadDir =  path
-        next()
-    }
-
-}
+  return (req, res, next) => {
+    req.uploadDir = path;
+    next();
+  };
+};
 
 const myStorage = multer.diskStorage({
-
-    destination : (req,file,cb) => {
-
-        const path = "./public/uploads"+req.uploadDir
-        if(!fs.existsSync(path)){
-            fs.mkdirSync(path,{recursive:true})
-        }
-
-        cb(null, path)
-    },
-
-    filename: (req,file,cb) => {
-        const ext = file.filename.split(".").pop()
-        const filename = Date.now()+"-"+generateRandomString(20)+"."+ext
-        cb(null,filename)
+  destination: (req, file, cb) => {
+    const path = "./public/uploads" + req.uploadDir;
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
     }
-    
 
-})
+    cb(null, path);
+  },
+
+  filename: (req, file, cb) => {
+    const ext = file.filename.split(".").pop();
+    const filename = Date.now() + "-" + generateRandomString(20) + "." + ext;
+    cb(null, filename);
+  },
+});
+
+const imageFilter = (req, file, cb) => {
+  const ext = file.filename.split(".").pop();
+  const allowed = ["jpg", "jpeg", "png", "svg", "webp", "gif", "bmp"];
+  if (allowed.includes(ext.toLowerCase())) {
+    cb(null, true);
+  } else {
+    cb({ code: 400, message: "Image format not supported" });
+  }
+};
 
 const uploader = multer({
-
-    storage: myStorage,
-    
-})
+  storage: myStorage,
+  fileFilter: imageFilter,
+});

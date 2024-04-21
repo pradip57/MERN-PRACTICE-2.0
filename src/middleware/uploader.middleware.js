63bcebf6965f5bc81108a66cfb15a17d14ce80e4
Pradip <dellpidarp57@gmail.com>
2024-04-21 20:05:12 +0545

    first configuration : destination is added

--- src/middleware/uploader.middleware.js
+++ src/middleware/uploader.middleware.js
@@ -1,8 +1,26 @@
 const multer = require('multer')
+const fs = require('fs')
+
+const setPath = (path) => {
+    return (req,res,next) => {
+        req.uploadDir =  path
+        next()
+    }
+
+}
 
 const myStorage = multer.diskStorage({
 
-    
+    destination : (req,file,cb) => {
+
+        const path = "./public/uploads"+req.uploadDir
+        if(!fs.existsSync(path)){
+            fs.mkdirSync(path,{recursive:true})
+        }
+
+        cb(null, path)
+    },
+
 })
 
 const uploader = multer({

698ee4b5602d2599255630a5d730750a9ccc91e0
Pradip <dellpidarp57@gmail.com>
2024-04-24 07:00:47 +0545

    send email function configured in mail services

--- src/services/mail.services.js
+++ src/services/mail.services.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 class MailServices {
@@ -7,19 +7,35 @@ class MailServices {
   constructor() {
     try {
       this.transport = nodemailer.createTransport({
-
-        host:process.env.SMTP_HOST,
-        port:process.env.SMTP_PORT,
-        auth:{
-            user:process.env.SMTP_USERNAME,
-            pass:process.env.SMTP_PASSWORD
-        }
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
       });
     } catch (exception) {
       console.log(exception);
       throw new Error("Error connecting email services");
     }
   }
+
+  sendEmail = (to, subject, message, attachments = null) => {
+    try {
+      const mailStatus = this.transport.sendMail({
+        to: to,
+        from: process.env.SMTP_FROM,
+        subject: subject,
+        html: message,
+        attachments:attachments
+      });
+      console.log(mailStatus)
+      return mailStatus
+    } catch (exception) {
+      console.log(exception);
+      throw new Error("Error sending mail......");
+    }
+  };
 }
 
 const mailServc = new MailServices();

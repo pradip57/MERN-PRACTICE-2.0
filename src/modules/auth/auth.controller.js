6796dfc4ad35e87cfe093c3fe08b8b55bf0ee8a3
Pradip <dellpidarp57@gmail.com>
2024-04-24 07:21:51 +0545

    password encrypted using bcryptjs package and payload manipulation

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,19 +1,23 @@
 const Joi = require("joi");
+const { generateRandomString } = require("../../utilities/helpers");
+const bcryptjs = require("bcryptjs");
 
 class AuthController {
   register = async (req, res, next) => {
-    try{
+    try {
       const payload = req.body;
 
-    
+      payload.password = bcryptjs.hashSync(payload.password, 10);
+      payload.status = "inactive";
+      payload.activationToken = generateRandomString(50);
 
-    res.json({
-      result: payload,
-      message: "Successful Register",
-      meta: null,
-    });
-    }catch(exception){
-      next(exception)
+      res.json({
+        result: payload,
+        message: "Successful Register",
+        meta: null,
+      });
+    } catch (exception) {
+      next(exception);
     }
   };
 

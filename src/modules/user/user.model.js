713540f2a87fb3c30bf8b801aa100b587d113a8b
Pradip <dellpidarp57@gmail.com>
2024-05-05 11:47:14 +0545

    user model created

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = new mongoose.Schema({
+  houseNo: String,
+  streetName: String,
+  ruralDev: String,
+  district: String,
+  province: String,
+});
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    min: 2,
+    max: 50,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+
+  role: {
+    type: String,
+    enum: ["seller", "admin", "customer"],
+    default: "customer",
+  },
+
+  activationToken: String,
+
+  status: {
+    type: String,
+    enum: ["inactive", "active"],
+    default: "inactive",
+  },
+  phone: String,
+  image: String,
+  address: {
+    shippingAddress: AddressSchema,
+    billingAddress: AddressSchema,
+  },
+
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+
+  updatedBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    default: null,
+  }
+},{
+    timestamps:true,
+    autoCreate:true,
+    autoIndex:true
+
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = UserModel;

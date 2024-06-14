ffe970d12cf0947d84ff7d39739554aeeb3ac62d
Pradip <dellpidarp57@gmail.com>
2024-06-14 16:36:18 +0545

    cart detail model done

---
+++ src/modules/cart/cart-detail.model.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const CartDetailSchema = new mongoose.Schema(
+  {
+    buyerId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    productId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      require: true,
+    },
+    orderId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Order",
+      default: null,
+    },
+    productDetails: {
+      title: String,
+      price: Number,
+      discount: Number,
+      afterDiscount: Number,
+    },
+    quantity: {
+      type: Number,
+      require: true,
+      min: 1,
+    },
+    amount: {
+      type: Number,
+    },
+    sellerId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "ordered", "cancelled"],
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: String,
+      ref: "User",
+      default: null,
+    },
+    updatedBy: {
+      type: String,
+      ref: "User",
+      default: null,
+    },
+  },
+  { timestamps: true, autoCreate: true, autoIndex: true }
+);
+
+const CartDetailModel = mongoose.model("CartDetail", CartDetailSchema);
+
+module.exports = CartDetailModel;
